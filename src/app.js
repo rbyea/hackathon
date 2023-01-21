5aa603e15ca9339e5f9d1299eeee8be0e7d144fe
Nikola_Antonov <Android2.1666@email.com>
2023-01-21 20:14:09 +0500

    added a new class file and added styles for the class

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ import { ContextMenu } from './menu.js'
 import { BackgroundModule } from './modules/background.module'
 import { ClicksModule } from './modules/clicks.module'
 import { ShapeModule } from './modules/shape.module'
+import { CustomMsg } from "./modules/customMsg"
 // следующие модули
 
 const backgroundModule = new BackgroundModule('background', 'Поменять цвет')
 const clicksModule = new ClicksModule('clicks', 'Считать клики (3 сек)')
 const shapeModule = new ShapeModule('shape', 'Создать фигуру')
+const customMsg = new CustomMsg('msg', 'Кастомное сообщение')
 // следующие модули
 
 const contextMenu = new ContextMenu('.menu')
@@ -17,6 +19,7 @@ const contextMenu = new ContextMenu('.menu')
 contextMenu.add(backgroundModule)
 contextMenu.add(clicksModule)
 contextMenu.add(shapeModule)
+contextMenu.add(customMsg)
 // следующие модули
 
 document.addEventListener('contextmenu', event => {
@@ -38,6 +41,10 @@ menu.addEventListener('click', event => {
     }
     if (menuItem.dataset.type === 'shape') {
       shapeModule.trigger()
+      contextMenu.close()
+    }
+    if (menuItem.dataset.type === 'msg') {
+      customMsg.trigger()
       contextMenu.close()
     }
     // следующие модули
