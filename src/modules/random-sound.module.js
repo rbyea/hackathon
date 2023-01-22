b85a737288fa1307f48fec0868ba4a65de4c6e81
FredFY12 <fred-fy12@yandex.ru>
2023-01-22 10:56:10 +0200

    !

--- src/modules/random-sound.module.js
+++ src/modules/random-sound.module.js
@@ -1,11 +1,20 @@
 import { Module } from '../core/module'
 import { getRandomArrayElement } from '../utils'
-import { sound } from '../settings'
 
 export class RandomSoundModule extends Module{
   trigger(){
-const classAudio = document.querySelector(".audio")
-document.body.insertAdjacentHTML('beforeend',`<audio class="audio" src="${getRandomArrayElement(sound)}" autoplay></audio>`)
-classAudio.remove()
+    fetch("https://62c1d18c2af60be89ece4372.mockapi.io/test")
+    .then(res => res.json())
+    .then(res => {
+      res.forEach((arr) => {
+        const arrayRandomSounds = arr.sounds
+        const classAudio = document.querySelector(".audio")
+        document.body.insertAdjacentHTML('beforeend',`<audio class="audio" src="${getRandomArrayElement(arrayRandomSounds)}" autoplay></audio>`)
+        if(classAudio){
+          classAudio.remove()
+        }      
+      }) 
+    })
+    .catch(error => alert('Произошла ошибка в получении данных о звуках...'))
   }
 }
--- src/settings.js
+++
@@ -1,6 +0,0 @@
-export const sound = [
-  'https://actions.google.com/sounds/v1/cartoon/cartoon_boing.ogg',
-  'https://actions.google.com/sounds/v1/cartoon/cartoon_cowbell.ogg',
-  'https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg',
-  'https://actions.google.com/sounds/v1/cartoon/concussive_hit_guitar_boing.ogg' 
-]
